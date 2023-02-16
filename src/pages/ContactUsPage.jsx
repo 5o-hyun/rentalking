5002eb5076b73b092ac3d36515e8896373e38324
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2023-02-16 12:31:53 +0900

    Feat: kakao map

--- src/pages/ContactUsPage.jsx
+++ src/pages/ContactUsPage.jsx
@@ -10,8 +10,37 @@ import {
   AiOutlineTwitter,
 } from "react-icons/ai";
 import { SiVimeo } from "react-icons/si";
+import { useEffect } from "react";
+
+const { kakao } = window;
 
 const ContactUsPage = () => {
+  useEffect(() => {
+    //지도를 담을 영역의 DOM 레퍼런스
+    const container = document.getElementById("map");
+    //지도를 생성할 때 필요한 기본 옵션
+    const options = {
+      center: new kakao.maps.LatLng(37.29432708770308, 126.99658273529194), //지도의 중심좌표.
+      level: 3, //지도의 레벨(확대, 축소 정도)
+    };
+    //지도 생성 및 객체 리턴
+    const map = new kakao.maps.Map(container, options);
+
+    // 마커가 표시될 위치입니다
+    var markerPosition = new kakao.maps.LatLng(
+      37.29432708770308,
+      126.99658273529194
+    );
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+      position: markerPosition,
+    });
+
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
+  }, []);
+
   return (
     <DefaultLayout>
       <Subject>get in touch</Subject>
@@ -48,7 +77,7 @@ const ContactUsPage = () => {
               <AiOutlineTwitter className="disabled" />
             </a>
           </div>
-          <div className="map"></div>
+          <div id="map"></div>
         </Left>
         <Right>
           <p className="questionTitle">Leave us a message</p>
@@ -130,7 +159,7 @@ const Left = styled.div`
       }
     }
   }
-  .map {
+  #map {
     height: 22rem;
     background-color: crimson;
     @media screen and (max-width: 767px) {
